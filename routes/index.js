7460ff40100f7ac7482b50a0c16b7516bc2d265a
Vedavyas <rvn582@gmail.com>
2021-04-11 00:47:33 +0530

    heroku setup

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ const router = require("express").Router();
 
 const postsHandler = require("./posts");
 const approvalHandler = require("./approval");
+router.get('/',(req,res,next)=>{res.send('Yayy!!')})
 router.use("/posts", postsHandler);
 router.use("/approvals", approvalHandler);
 module.exports = exports = router;
